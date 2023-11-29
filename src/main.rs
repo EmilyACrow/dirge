657df9f209c235842bf44f7787aee097fa0d0e89
Emily Crow <emilycrow@protonmail.com>
2023-11-29 13:35:52 -0600

    Experimenting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((DefaultPlugins,HelloPlugin))
+        .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+    commands.spawn((Person, Name("Jermaine Lawrence".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<(&Person, &Name)>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for (_person, name) in &mut query.iter() {
+            println!("Hello {}!", name.0);
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
+    }
 }
